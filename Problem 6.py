1bb5e32f2f6f07ef6fdb56e1ba4cf1c8ec96d7f4
PurelyStupidity <micoolplays@gmail.com>
2023-02-08 20:02:45 +0000

    Feb 9, 2023, 9:02 AM

--- Problem 6.py
+++ Problem 6.py
@@ -2,7 +2,7 @@ numbers = [20, 36, 12, 24, 20, 48, 74, 353, 23, 98]
 
 largest = 0
 
-for i in range(len(numbers)):
-    if numbers[i] > largest:
-        largest = numbers[i]
+for number in numbers:
+    if number > largest:
+        largest = number
 print(largest)
