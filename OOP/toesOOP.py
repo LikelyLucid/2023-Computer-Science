2570a85d35b477557154f5a8778948bc1b629caa
PurelyStupidity <micoolplays@gmail.com>
2023-02-28 02:09:30 +0000

    Feb 28, 2023, 3:09 PM

--- OOP/toesOOP.py
+++ OOP/toesOOP.py
@@ -1,12 +1,4 @@
 class Toes:
     def __init__(self, size):
         self.size = size
-        self.count = 0
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.count < self.size:
-            self.count += 1
-            return self.count
-        else:
-            raise StopIteration
+        self.count =
