b7aff51e0fdf6513970852f737c5ce96383fc20d
PurelyStupidity <micoolplays@gmail.com>
2023-02-28 02:11:20 +0000

    Feb 28, 2023, 3:11 PM

--- OOP/toesOOP.py
+++ OOP/toesOOP.py
@@ -1,2 +1,10 @@
 class Toes:
-    def __init__(self, sexyness, aroma, 
+    def __init__(self, sexyness, aroma, flavor, texture):
+        self.sexyness = sexyness
+        self.aroma = aroma
+        self.flavor = flavor
+        self.texture = texture
+    def __str__(self):
+        return f"{self.sexyness} {self.aroma} {self.flavor} {self.texture}"
+    def __repr__(self):
+        return f"{self.sexyness} {self.aroma} {self.flavor} {self.texture}"
