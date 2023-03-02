c1a9a19539ec69bc275b9fc9990739f18042c383
PurelyStupidity <micoolplays@gmail.com>
2023-03-02 22:24:59 +0000

    Mar 3, 2023, 11:24 AM

--- OOP/Superclass.py
+++ OOP/Superclass.py
@@ -1,2 +1,9 @@
 class worker:
-    def __init__(self, name, age, id, birthdate, job
+    def __init__(self, name, age, id, birthdate, job):
+        self.name = name
+        self.age = age
+        self.id = id
+        self.birthdate = birthdate
+        self.job = job
+    def __str__(self):
+        return f"{self.name} {self.age} {self.id} {self.birthdate} {self.job}"
