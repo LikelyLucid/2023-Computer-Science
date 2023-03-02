eb8e3052680304d39066c12482c4e657c7ea1e01
PurelyStupidity <micoolplays@gmail.com>
2023-03-02 22:28:13 +0000

    Mar 3, 2023, 11:28 AM

--- OOP/Superclass.py
+++ OOP/Superclass.py
@@ -7,4 +7,10 @@ class worker:
         self.job = job
     def __str__(self):
         return f"{self.name} {self.age} {self.id} {self.birthdate} {self.job}"
-class managment(worker)
+class managment(worker):
+    def __init__(self, name, age, id, birthdate, job, salary, bonus):
+        super().__init__(name, age, id, birthdate, job)
+        self.salary = salary
+        self.bonus = bonus
+    def __str__(self):
+        return f"{self.name} {self.age} {self.id} {self.birthdate} {self.job} {self.salary} {self.bonus}"
