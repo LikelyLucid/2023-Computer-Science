185030227c60989e1ba6b5caceb8eb2e85d8a473
PurelyStupidity <micoolplays@gmail.com>
2023-03-02 22:22:36 +0000

    Mar 3, 2023, 11:22 AM

--- OOP/Superclass.py
+++ OOP/Superclass.py
@@ -1,12 +1,2 @@
 class worker:
-    def __init__(self, name, age, pay):
-        self.name = name
-        self.age = age
-        self.pay = pay
-        self.earnings = 0
-
-    def lastname(self):
-        return self.name.split()[-1]
-
-    def give_raise(self, percent):
-        self.pay *= (1.0 + percent)
+    def __init__(self, name, age
