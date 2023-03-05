7eec9d31fcd1652c059066a3691ef7715cb9684d
PurelyStupidity <micoolplays@gmail.com>
2023-03-05 23:20:01 +0000

    Mar 6, 2023, 12:20 PM

--- OOP/Library.py
+++ OOP/Library.py
@@ -7,8 +7,9 @@ class Book:
         self.available = True
         self.borrower = None
         book_list.append(self)
-        
-    def book_details(self)
+
+    def book_details(self):
+        print()
 
 book_list = []
 Book("Lord of the Rings", "J. R. R. Tolkien", "TOL", "082409640")
