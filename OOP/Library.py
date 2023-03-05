1776a8f9eb4bb07b52a937b22df7b2d081d65cac
PurelyStupidity <micoolplays@gmail.com>
2023-03-06 12:18:34 +1300

    jnj

--- OOP/Library.py
+++ OOP/Library.py
@@ -1,8 +1,15 @@
 class Book:
-    __init__(self, title, author, dewey, isbn):
-    self.title = title. title() # string with capitalised first letter
-    self.author = author # string
-    self.dewey = dewey # string
-    self.isbn = isbn # string
-    self.available = True
-    self.borrower = None
+    def __init__(self, title, author, dewey, isbn):
+        self.title = title. title() # string with capitalised first letter
+        self.author = author # string
+        self.dewey = dewey # string
+        self.isbn = isbn # string
+        self.available = True
+        self.borrower = None
+        book_list.append(self)
+
+book_list = []
+Book("Lord of the Rings", "J. R. R. Tolkien", "TOL", "082409640")
+Book("The Hunger Games", "Suzzane Collins", "COL", "095395267")
+Book("A Tale Of Two Cities", "Charles Dickens", "DIK", "60462809248")
+Book("Harry Potter And The Sorcerer's Stone", "J. K. Rowling", "ROW", "9781461245678")
