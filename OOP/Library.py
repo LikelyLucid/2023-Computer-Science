4342918ec55eb5c3879c73a354bc1d2baa811d89
PurelyStupidity <micoolplays@gmail.com>
2023-03-05 23:12:13 +0000

    Mar 6, 2023, 12:12 PM

--- OOP/Library.py
+++ OOP/Library.py
@@ -1,5 +1,5 @@
 class Book:
-    init (self, title, author, dewey, isbn):
+    init(self, title, author, dewey, isbn):
     self.title = title. title() # string with capitalised first letter
     self.author = author # string
     self.dewey = dewey # string
@@ -7,3 +7,13 @@ class Book:
     self. available = True
     self. borrower = None
 
+Book("
+of the Rings",
+"J.R.R.ToUien", "TOL", "97802611032S2")
+Hunger Games", "Suzanne Cottins", "COL
+, "9781407132082")
+A Tate Of Two Cities"
+"Charles Dickens"
+, "DIC", "9781853262647")
+"RON", "9780439321624")
+Potter"
