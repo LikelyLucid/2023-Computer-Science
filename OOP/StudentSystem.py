2a5d6fd6fb475f2983a90e5c64ebecb4dd0b3db5
PurelyStupidity <micoolplays@gmail.com>
2023-03-08 20:23:51 +0000

    Mar 9, 2023, 9:23 AM

--- OOP/StudentSystem.py
+++ OOP/StudentSystem.py
@@ -26,10 +26,7 @@ def generate_students():
     with open('random_students.csv', newline='') as csvfile:
         filereader = csv.reader(csvfile, delimiter='|')
         for line in filereader:
-            if line[5] == "True":
-                is_male = True
-            else:
-                is_male = False
+            is_male = line[5] == "True"
             Student(line[0], int(line[1]), line[2], line[3], line[4], is_male)
 
 def print_student_details():
@@ -43,12 +40,12 @@ def select_student_age():
             student.display_info()
 
 def count_in_class():
+    class_name = input("Enter the name of the class: ").upper()
     count = 0
-    class_name = input("Enter the class: ").upper()
     for student in student_list:
         if student.formclass == class_name:
             count += 1
-    print(f"{class} has {count} students")
+    print(f"{class_name} has {count} students")
 
 student_list = []
 generate_students()
