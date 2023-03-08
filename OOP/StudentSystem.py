79e6d0ef7ef206a68d89006b2f5b6e9aec9a3ae3
PurelyStupidity <micoolplays@gmail.com>
2023-03-08 20:12:33 +0000

    Mar 9, 2023, 9:12 AM

--- OOP/StudentSystem.py
+++ OOP/StudentSystem.py
@@ -7,6 +7,7 @@ class Student:
         self.subjects = subjects
         self.is_male = True
         self.enrolled = True
+        student_list.append(self)
 
     def display_info(self):
         print(f"Name: {self.name}")
@@ -17,11 +18,11 @@ class Student:
         print(f"Is Male: {self.is_male}")
         print(f"Enrolled: {self.enrolled}")
 
-# Karen, 17, 123-4567,WNLR, 13DTC,13SMX, female
-# Bob, 18, 021-0263674,BNNL, 13SMX, male
-# Lisa,16, 022-4567123,SKWR, 13DTC,13SMX, female
-# Patrick, 18, 023-01234567, SCBE, 13ENG, 13CMX, 13SMX, 13DTC, male
+def print_student_details():
+    for student in student_list:
+        student.display_info()
 
+student_list = []
 Student("Karen", 17, "123-4567", "WNLR", ("13DTC", "13SMX"), False)
 Student("Bob", 18, "021-0263674", "BNNL", ("13SMX", "13ENG"), True)
 Student("Lisa", 16, "022-4567123", "SKWR", ("13DTC", "13CMX"), False)
