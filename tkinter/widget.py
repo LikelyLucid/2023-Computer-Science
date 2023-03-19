edacf7f0b1be2eaf05cdddc62c81aff345b9a8a4
PurelyStupidity <micoolplays@gmail.com>
2023-03-19 23:18:13 +0000

    Mar 20, 2023, 12:18 PM

--- tkinter/widget.py
+++ tkinter/widget.py
@@ -1,11 +1,11 @@
 from tkinter import *
 root = Tk()
 
-root.maxsize("800x200")
+root.maxsize(800, 200)
 root.title("main window")
 
 root.geometry("600x200")
 greeting = Label(root, text="Hello")
 
 greeting.pack()
-
+root.mainloop()
